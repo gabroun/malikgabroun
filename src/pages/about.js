3ae647ba70d94f3c7a6efe3a5b877872c115c5a9
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-10-13 13:00:46 +0100

    prettier config

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <div style={{ color: `red` }}>
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+    <Link to="/">Home</Link>
+  </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 var React = require("react");
+import { Link } from "gatsby";
 var Header = require("../components/Header");
 
 export default () => (
   <div style={{ color: `purple` }}>
     <Header />
-    <h1>Hello Gatsby!!</h1>
+    <Link to="/about">About</Link>
+    <h1>Hello Gatsby!!!</h1>
     <p>What a world.</p>
   </div>
 );
