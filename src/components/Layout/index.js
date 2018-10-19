f3ab57b14cbe6929e065b9dcb9713a015619448b
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-10-19 07:22:07 +0100

    first graphql query test

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,13 +9,32 @@ class Layout extends React.Component {
     const { children } = this.props;
     return (
       <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 650,
-          padding: `0 1rem`,
-          color: `red`,
-        }}
+        className={css`
+          margin: 0 auto;
+          max-width: 700px;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1.5)};
+        `}
       >
+        <Link to={`/`}>
+          <h3
+            className={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+            `}
+          >
+            Pandas Eating
+          </h3>
+        </Link>
+        <Link
+          to={`/about/`}
+          className={css`
+            float: right;
+          `}
+        >
+          About
+        </Link>
         {children}
       </div>
     );
