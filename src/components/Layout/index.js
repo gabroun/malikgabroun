29950fc21f5da30306de03e93125a92b91d8e030
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2018-10-22 22:18:19 +0100

    blog component style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -11,30 +11,10 @@ class Layout extends React.Component {
       <div
         className={css`
           margin: 0 auto;
-          max-width: 700px;
-          padding: ${rhythm(2)};
+
           padding-top: ${rhythm(1.5)};
         `}
       >
-        <Link to={`/`}>
-          <h3
-            className={css`
-              margin-bottom: ${rhythm(2)};
-              display: inline-block;
-              font-style: normal;
-            `}
-          >
-            Pandas Eating
-          </h3>
-        </Link>
-        <Link
-          to={`/about/`}
-          className={css`
-            float: right;
-          `}
-        >
-          About
-        </Link>
         {children}
       </div>
     );
