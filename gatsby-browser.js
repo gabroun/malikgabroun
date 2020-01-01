06c4f75fc97cb01abfedb567e641dcf86f7b7c91
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2020-01-01 12:09:47 +0000

    adding syntax highlighting to mdx pages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,41 @@
 require('prismjs/themes/prism-solarizedlight.css');
 require('typeface-rambla');
+
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+
+/* eslint-disable */
+const component = {
+  pre: props => {
+    const className = props.children.props.className || '';
+    const matches = className.match(/language-(?<lang>.*)/);
+    return (
+      <Highlight
+        {...defaultProps}
+        code={props.children.props.children}
+        language={
+          matches && matches.groups && matches.groups.lang
+            ? matches.groups.lang
+            : ''
+        }
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
+    );
+  },
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={component}>{element}</MDXProvider>;
+};
