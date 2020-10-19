6a66ac516b0e5d2ff7c30373c00e0f8d8133b989
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2020-10-19 21:14:26 +0100

    updating onchange and state of form - wip

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,6 +3,16 @@ import * as S from './styles'
 import React from 'react'
 
 class ContactForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {firstname: "", lastname: "", email: "", message: ""}
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    } 
     render() {
         return (
             <S.ContactUsForm name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
@@ -11,20 +21,20 @@ class ContactForm extends React.Component {
                 <div className="contact-name">
                     <p className="flex-col">
                         <label htmlFor="firstname">First Name</label>
-                        <input type="text" name="firstname" />
+                        <input type="text" name="firstname" value={this.state.firstname} onChange={this.handleChange} />
                     </p>
                     <p className="flex-col">
                         <label htmlFor="lastname">Last Name</label>
-                        <input type="text" name="lastname" />
+                        <input type="text" name="lastname" value={this.state.lastname} onChange={this.handleChange} />
                     </p>
                 </div>
                 <div className="flex-col">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email"/>
+                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/>
                 </div>
                 <div className="flex-col">
                     <label htmlFor="message">Message</label>
-                    <textarea name="message" />
+                    <textarea name="message" value={this.state.message} onChange={this.handleChange} />
                 </div>
                 <button type="submit" className="contact-submit">Submit</button>
             </S.ContactUsForm>
