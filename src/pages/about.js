e4dff883c8c1c846fc80950c602ec0924230b83a
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2020-10-30 17:47:42 +0000

    updating about page to include contact form and carousel instead of timeline

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,14 @@
 import * as S from '../components/styles/about';
 
-import { Event, Timeline } from '../components/Timeline';
 import React, { Fragment } from 'react';
 
-import ContactForm from '../components/ContactForm'
+import Carousel from '../components/Carousel';
+import ContactForm from '../components/ContactForm';
 import Layout from '../components/Layout';
-import Socials from '../components/Socials'
+import Socials from '../components/Socials';
 import { ThemeProvider } from 'styled-components';
-import le from '../../static/about/leicester.jpg';
-import rb from '../../static/about/redbox.jpg';
-import su from '../../static/about/surrey.png';
+import headImg from '../resources/headShot.png';
 import theme from '../components/styles/theme';
-import ur from '../../static/about/unirazak1.png';
 
 class About extends React.Component {
   render() {
@@ -20,48 +17,45 @@ class About extends React.Component {
         <Layout title="About">
           <div className="main-content">
             <Fragment>
-              <Timeline>
-                <Event
-                  dates={'2016 – preset'}
-                  title={'Front End Developer'}
-                  subtitle={'Redbox Digital'}
-                  logo={rb}
-                  position="right"
-                />
-                <Event
-                  dates={'2015 – 2016'}
-                  title={'MSc Software Engineering'}
-                  subtitle={'University of Leicester'}
-                  position="left"
-                  logo={le}
-                />
-                <Event
-                  dates={'2014 – 2015'}
-                  title={'MSc International Business Management'}
-                  subtitle={'University of Surrey'}
-                  position="right"
-                  logo={su}
-                />
-                <Event
-                  dates={'2010 – 2013'}
-                  title={'BSc Information Technology'}
-                  subtitle={'University Tun Abdul Razak'}
-                  position="left"
-                  logo={ur}
-                />
-              </Timeline>
+              <S.AboutMe className="about-me">
+                <div className="intro">
+                  <h3>About Me</h3>
+                  <p>Hello, my name is Malik Elgabroun.</p>
+
+                  <p>
+                    I’m a Front end Developer based in London 🇬🇧. Currently
+                    Working as a Front end Developer at Redbox Digital.
+                  </p>
+
+                  <p>
+                    I enjoy writing software and building application, and
+                    currently I get to develop using React, Vue, TypeScript and
+                    many more.
+                  </p>
+
+                  <p>
+                    My hobbies are doing sports from playing football, running
+                    and cycling. Also I like travelling to new places when the
+                    opportunity arises.
+                  </p>
+                </div>
+                <img src={headImg} />
+              </S.AboutMe>
+              <Carousel />
               <S.ContactUs>
                 <div className="social">
                   <h1>Get In Touch</h1>
-                  <p>Want to ask a question, have a chat or ask about work or anything else? Send me a message! and I will try to responsed as quickly as I can.
+                  <p>
+                    Want to ask a question, have a chat or ask about work or
+                    anything else? Send me a message! and I will try to
+                    responsed as quickly as I can.
                   </p>
-                  <Socials/>
+                  <Socials />
                 </div>
                 <div className="contact-form">
-                <ContactForm/>
+                  <ContactForm />
                 </div>
               </S.ContactUs>
-              
             </Fragment>
           </div>
         </Layout>
